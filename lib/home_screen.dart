ad28c2d9fef8e68dca6a531bf4d1c5719aa7a3e0
Tetiana Tortykh <t.tortykh@gmail.com>
2023-10-15 23:14:34 +0300

    part of settings and main screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,93 @@
+import 'package:cafe_app/account_screen.dart';
 import 'package:flutter/material.dart';
+
+class HomeNavigation extends StatefulWidget {
+  const HomeNavigation({super.key});
+
+  @override
+  State<HomeNavigation> createState() => _HomeNavigationState();
+}
+
+class _HomeNavigationState extends State<HomeNavigation> {
+  int _selectedTab = 0;
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  final List _pages = const [
+    HomeScreen(),
+    Center(
+      child: Text('Order'),
+    ),
+    AccauntScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: _pages[_selectedTab],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) => _changeTab(index),
+        selectedItemColor: const Color.fromARGB(255, 48, 28, 27),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.short_text_rounded), label: "Order"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+        ],
+      ),
+    );
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static const String testImage = 'lib/global/assets/pepper.jpg';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [Text('bla cafe')],
-      )),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: const Text('Cafe Bla'),
+          expandedHeight: 200.0,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              testImage,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(
+                10,
+                (index) {
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Image.asset(testImage),
+                      ),
+                      Text('Category $index')
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
