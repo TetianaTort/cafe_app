ed6e3314fd4bdaed6c7a26b727ced4be4504d104
Tetiana Tortykh <t.tortykh@gmail.com>
2023-10-16 12:01:35 +0300

    locale + theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:cafe_app/home_screen.dart';
+import 'package:cafe_app/model/settings.dart';
+import 'package:cafe_app/utils/custom_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,17 +12,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cafe app',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => SettingsModel(),
+      child: Consumer<SettingsModel>(
+        builder: (context, settingsModel, child) => MaterialApp(
+          title: 'Cafe app',
+          debugShowCheckedModeBanner: false,
+          theme: settingsModel.isLightTheme ? CustomTheme.lightTheme : CustomTheme.darkTheme,
+          // theme: CustomTheme.lightTheme,
+          // darkTheme: CustomTheme.darkTheme,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: settingsModel.locale,
+          home: const HomeNavigation(),
+        ),
       ),
-      home: const HomeNavigation(),
     );
   }
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Cafe app',
+  // debugShowCheckedModeBanner: false,
+  // theme: CustomTheme.lightTheme,
+  // darkTheme: CustomTheme.darkTheme,
+  //     localizationsDelegates: const [
+  //       AppLocalizations.delegate,
+  //       GlobalMaterialLocalizations.delegate,
+  //       GlobalWidgetsLocalizations.delegate,
+  //       GlobalCupertinoLocalizations.delegate,
+  //     ],
+  //     supportedLocales: const [
+  //       Locale('en'),
+  //       Locale('uk'),
+  //     ],
+  //     home: const HomeNavigation(),
+  //   );
+  // }
 }
